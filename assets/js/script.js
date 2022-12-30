9a7f693c21a909f46a37e04e04a107a8710c24ed
poncianodev <lucasbponciano@hotmail.com>
2022-12-30 03:02:36 -0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function botaoWhats() {
+    window.open("https://wa.me/qr/7VANEGJJVP6YL1", "_blank");
+}
+
+function botaoInsta() {
+    window.open("https://www.instagram.com/poncianodev/", "_blank");
+}
+
+function botaoGit() {
+    window.open("https://github.com/poncianodev", "_blank");
+}
+
+function botaoSite() {
+    window.open(
+        "https://poncianodev.github.io/portfolio-poncianodev/",
+        "_blank",
+    );
+}
